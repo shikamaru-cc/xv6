2deb558cfa3ca17bf0e8e93a074beedbc671f495
shikamaru <shikamaru.cc@proton.me>
2025-04-14 20:37:07 +0800

    primes: refine

--- user/primes.c
+++ user/primes.c
@@ -1,96 +1,95 @@
+/*
+    p = get a number from left neighbor
+    print p
+    loop:
+        n = get a number from left neighbor
+        if (p does not divide n)
+            send n to right neighbor
+*/
+
 #include "kernel/types.h"
 #include "user/user.h"
 
-typedef enum {
-    false,
-    true
-} bool;
+static int pleft = 0;
+static int pright = 0;
 
-bool 
-isPrime(int a)
-{
-    for (int i = 2; i <= a/2; i++) {
-        if ((a % i) == 0) {
-            return false;
-        }
-    }
-    return true;
-}
+void do_primes() __attribute__((noreturn));
 
-void
-runFilter(int pRead[2], int pWrite[2], int feed)
-{
-    char num[5];
-    char c;
-    int n;
+void do_primes() {
+    int p, n;
 
-    int i = 0;
-    while(read(pRead[0], &c, 1) != 0) {
-        num[i++] = c;
-        if(c != '\0') {
+    read(pleft, &p, sizeof(int));
+    fprintf(1, "prime %d\n", p);
+
+    while (read(pleft, &n, sizeof(int)) > 0) {
+        if (n % p == 0) {
             continue;
         }
-        n = atoi(num);
-        if(n == feed) {
-            fprintf(1, "prime %d\n", n);
-        } else if(n % feed != 0) {
-            write(pWrite[1], num, strlen(num)+1);
-        }
-        i = 0;
-    }
-}
 
-int
-main(int argc, char *argv[])
-{
-    int wg = 0;
-    int pWrite[2], pRead[2], pMain[2];
+        // spawn right
+        if (!pright) {
+            int pipefd[2];
+            if (pipe(pipefd) < 0) {
+                fprintf(2, "failed to pipe\n");
+                exit(1);
+            }
 
-    pipe(pWrite);
-    // pMain is the write pipe for main routine
-    memcpy(pMain, pWrite, 2*sizeof(int));
+            int pid = fork();
+            if (pid < 0) {
+                fprintf(1, "failed to fork\n");
+                exit(1);
+            }
 
-    for(int i = 2; i <= 35; i++) {
-        if(isPrime(i)) {
-            memcpy(pRead, pWrite, 2*sizeof(int));
-            pipe(pWrite);
-            if(fork() == 0) {
-                // close main pipe
-                if(pRead[0] != pMain[0] && pRead[1] != pMain[1]) {
-                    close(pMain[0]);
-                    close(pMain[1]);
-                } 
-                close(pRead[1]);
-                close(pWrite[0]);
-
-                runFilter(pRead, pWrite, i);
-
-                close(pRead[0]);
-                close(pWrite[1]);
-
-                exit(0);
+            if (pid == 0) {
+                close(pipefd[1]);
+                close(pleft);
+                pleft = pipefd[0];
+                do_primes();
             } else {
-                // close read pipe
-                // reserve write pipe for next routine
-                if(pRead[0] != pMain[0] && pRead[1] != pMain[1]) {
-                    close(pRead[0]);
-                    close(pRead[1]);
-                }
-                wg++;
+                close(pipefd[0]);
+                pright = pipefd[1];
             }
         }
+
+        write(pright, &n, sizeof(int));
     }
 
-    char num[5];
-    for(int i = 2; i <= 35; i++) {
-        itoa(i, num);
-        write(pMain[1], num, strlen(num)+1);
-    }
-    close(pMain[0]);
-    close(pMain[1]);
+    close(pleft);
 
-    for(int i = 0; i < wg; i++) {
+    if (pright) {
+        close(pright);
         wait(0);
     }
+
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
+    int pipefd[2];
+    if (pipe(pipefd) < 0) {
+        fprintf(2, "failed to pipe\n");
+        exit(1);
+    }
+
+    int pid = fork();
+    if (pid < 0) {
+        fprintf(2, "failed to fork\n");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        close(pipefd[1]);
+        pleft = pipefd[0];
+        do_primes();
+    } else {
+        close(pipefd[0]);
+        pright = pipefd[1];
+    }
+
+    for (int i = 2; i <= 280; i++)
+        write(pright, &i, sizeof(int));
+
+    close(pright);
+    wait(0);
     exit(0);
 }
