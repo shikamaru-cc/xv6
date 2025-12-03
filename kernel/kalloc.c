1d808d3d1350e9ea28218c474873f2012ff0340d
shikamaru <shikamaru.cc0@gmail.com>
2025-12-03 18:25:51 +0800

    mem allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,6 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+void kfree_i(void *pa, uint i);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
@@ -23,11 +24,28 @@ struct {
   struct run *freelist;
 } kmem;
 
+struct arena {
+  struct spinlock lock;
+  struct run *freelist;
+} arenas[NCPU];
+
+char arena_names[NCPU][128];
+
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  // initlock(&kmem.lock, "kmem");
+  // freerange(end, (void*)PHYSTOP);
+
+  for(int i = 0; i < NCPU; i++){
+    // snprintf(arena_names[i], 128, "kmem %d", i);
+    initlock(&arenas[i].lock, "kmem");
+  }
+
+  char *p;
+  p = (char*)PGROUNDUP((uint64)end);
+  for(; p + PGSIZE <= (char*)PHYSTOP; p += PGSIZE)
+    kfree_i(p, ((uint64)p >> PGSHIFT) % NCPU);
 }
 
 void
@@ -39,14 +57,14 @@ freerange(void *pa_start, void *pa_end)
     kfree(p);
 }
 
-// Free the page of physical memory pointed at by pa,
-// which normally should have been returned by a
-// call to kalloc().  (The exception is when
-// initializing the allocator; see kinit above.)
 void
-kfree(void *pa)
+kfree_i(void *pa, uint i)
 {
   struct run *r;
+  struct arena *arena;
+
+  if(i >= NCPU)
+    panic("kfree_i: i >= NCPU");
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -55,11 +73,39 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
+  arena = arenas + i;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&arena->lock);
+  r->next = arena->freelist;
+  arena->freelist = r;
+  release(&arena->lock);
+}
+
+// Free the page of physical memory pointed at by pa,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(void *pa)
+{
+  kfree_i(pa, cpuid());
+}
+
+void *
+kalloc_i(int i)
+{
+  struct run *r;
+  struct arena *arena = &arenas[i];
+
+  acquire(&arena->lock);
+  r = arena->freelist;
+  if(r)
+    arena->freelist = r->next;
+  release(&arena->lock);
+
+  if(r)
+    memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,14 +115,16 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int i = cpuid();
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  if((r = kalloc_i(i)))
+    return r;
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  // steal
+  for(i = 0; i < NCPU; i++){
+    if((r = kalloc_i(i)))
+      break;
+  }
+
+  return r;
 }
--- kernel/spinlock.c
+++ kernel/spinlock.c
@@ -53,7 +53,7 @@ initlock(struct spinlock *lk, char *name)
   lk->nts = 0;
   lk->n = 0;
   findslot(lk);
-#endif  
+#endif
 }
 
 // Acquire the lock.
@@ -67,7 +67,7 @@ acquire(struct spinlock *lk)
 
 #ifdef LAB_LOCK
     __sync_fetch_and_add(&(lk->n), 1);
-#endif      
+#endif
 
   // On RISC-V, sync_lock_test_and_set turns into an atomic swap:
   //   a5 = 1
@@ -194,7 +194,7 @@ statslock(char *buf, int sz) {
       n += snprint_lock(buf +n, sz-n, locks[i]);
     }
   }
-  
+
   n += snprintf(buf+n, sz-n, "--- top 5 contended locks:\n");
   int last = 100000000;
   // stupid way to compute top 5 contended locks
@@ -211,7 +211,7 @@ statslock(char *buf, int sz) {
     last = locks[top]->nts;
   }
   n += snprintf(buf+n, sz-n, "tot= %d\n", tot);
-  release(&lock_locks);  
+  release(&lock_locks);
   return n;
 }
 #endif
